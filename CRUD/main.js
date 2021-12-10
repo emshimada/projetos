6ccce1770ce1d95412e06be0577306bd6669de53
emshimada <emshimada1991@gmail.com>
2021-12-09 22:57:49 -0300

    arquivo base

---
+++ CRUD/main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const openModal = () => document.getElementById('modal')
+    .classList.add('active')
+
+const closeModal = () => {
+    clearFields()
+    document.getElementById('modal').classList.remove('active')
+}
+
+  
+
+// Eventos
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', openModal)
+
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal)
