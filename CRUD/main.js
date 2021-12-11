3933126c21311f8fd39f3c5f72cea848e5cfb26f
emshimada <emshimada240891@gmail.com>
2021-12-11 10:04:47 -0300

    inclusão de cliente no localstorage

--- CRUD/main.js
+++ CRUD/main.js
@@ -1,18 +1,30 @@
 'use strict'
 
 const openModal = () => document.getElementById('modal')
-    .classList.add('active')
+    .classList.add('active');
 
 const closeModal = () => {
-    clearFields()
-    document.getElementById('modal').classList.remove('active')
+    clearFields();
+    document.getElementById('modal').classList.remove('active');
 }
 
-  
+const tempClient = {
+  nome : "Carla",
+  email : "massayuki_shimada@hotmail.com",
+  celular : "43984365454",
+  cidade : "Londrina"
+}
+
+const createClient = (client) => {
+  const db_client = JSON.parse(localStorage.getItem('db_client'));
+  console.log(db_client);
+  db_client.push(client);
+  localStorage.setItem("db_client", JSON.stringify(db_client));
+}
 
 // Eventos
 document.getElementById('cadastrarCliente')
-    .addEventListener('click', openModal)
+    .addEventListener('click', openModal);
 
 document.getElementById('modalClose')
-    .addEventListener('click', closeModal)
+    .addEventListener('click', closeModal);
