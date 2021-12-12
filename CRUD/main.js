1e1b3a4ee429776ef2a7644136ac3a7c0c54f1ce
Edu <emshimada240891@gmail.com>
2021-12-12 17:08:30 -0300

    functions de insert, delete, update e read

--- CRUD/main.js
+++ CRUD/main.js
@@ -10,17 +10,37 @@ const closeModal = () => {
 
 const tempClient = {
   nome : "Carla",
-  email : "shimada@hotmail.com",
+  email : "carla@hotmail.com",
   celular : "43984365454",
   cidade : "Londrina"
 }
-
-const createClient = (client) => {
-  const db_client = JSON.parse(localStorage.getItem('db_client'));
-  console.log(db_client);
-  db_client.push(client);
-  localStorage.setItem("db_client", JSON.stringify(db_client));
+//CRUD delete
+const deleteClient = (index) => {
+  const dbClient = readClient();
+  dbClient.splice(index, 1);
+  setLocalStorage(dbClient);
 }
+
+//CRUD update
+const updateClient = (index, client) => {
+  const dbClient = readClient();
+  dbClient[index] = client;
+  setLocalStorage(dbClient);
+}
+
+//CRUD read
+const readClient = () => getLocalStorage();
+
+// CRUD create
+const createClient = (client) => {
+  const dbClient = getLocalStorage();
+  dbClient.push(client);
+  setLocalStorage(dbClient); 
+  
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []; 
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient));
 
 // Eventos
 document.getElementById('cadastrarCliente')
@@ -28,3 +48,6 @@ document.getElementById('cadastrarCliente')
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal);
+    
+document.getElementById('salvar')
+    .addEventListener('click', saveClient);
